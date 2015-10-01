bfd6492fe87bca9b19771ea30758e736ee84a21d
AndrewNeo <andrew@andrewneo.com>
2015-10-01 04:53:45 -0700

    Make database path absolute for use with WSGI

--- database.py
+++ database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import os
 
-engine = create_engine("sqlite:///data/hybridius.sqlite", convert_unicode=True)
+
+db_file = "sqlite:///%s/data/hybridius.sqlite" % (os.path.dirname(__file__))
+
+engine = create_engine(db_file, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
